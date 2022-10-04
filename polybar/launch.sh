11bdd28b4b1b0e3a1c5306bbf5f50db75f11612e
iortego42P <iortego42madrid@gmail.com>
2022-10-04 17:14:19 +0200

    polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,18 +11,17 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 ## Launch
 
 ## custom.ini
-polybar metropolis -c ~/.config/polybar/custom.ini &
-polybar hackthebox -c ~/.config/polybar/custom.ini &
-polybar target -c ~/.config/polybar/custom.ini &
+polybar metropolis -c ~/.config/polybar/main.ini &
+polybar hackthebox -c ~/.config/polybar/main.ini &
+polybar target -c ~/.config/polybar/main.ini &
 
 ## internal.ini
-polybar ethernet -c ~/.config/polybar/internal.ini &
-polybar cpu -c ~/.config/polybar/internal.ini &
-polybar memory -c ~/.config/polybar/internal.ini &
+polybar ethernet -c ~/.config/polybar/main.ini &
+polybar cpu -c ~/.config/polybar/main.ini &
+polybar memory -c ~/.config/polybar/main.ini &
 
 ## essentials.ini
-polybar onoff_menu -c ~/.config/polybar/essentials.ini &
-polybar battery -c ~/.config/polybar/essentials.ini &
+polybar onoff_menu -c ~/.config/polybar/main.ini &
 
 ## workspace.ini
 polybar primary -c ~/.config/polybar/workspace.ini &
